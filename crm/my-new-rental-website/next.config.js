c2d825f04d48b2c9087b8fd5d62d6156bed5a216
BIDYUT PRABHA SAHU <bidyut@BIDYUTs-MacBook-Pro.local>
2024-12-29 19:48:16 -0600

    changes for images

--- crm/my-new-rental-website/next.config.js
+++ crm/my-new-rental-website/next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     unoptimized: true,
+    domains: ['localhost', 'omsmartstay.com'],
     remotePatterns: [
       {
         protocol: 'https',
@@ -9,7 +11,8 @@ const nextConfig = {
       },
     ],
   },
-  output: 'standalone',
+  // Add this for production
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://omsmartstay.com' : '',
 }
 
 module.exports = nextConfig
